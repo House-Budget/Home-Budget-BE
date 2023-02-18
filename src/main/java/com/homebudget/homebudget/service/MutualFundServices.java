b0bd444b999142d0516b7af4ab1eb47310e53e73
vaibhavgupta082 <vaibhavgupta2017082@gmail.com>
2023-02-18 19:07:38 +0530

    Adding exceptions to Mutual fund service

--- src/main/java/com/homebudget/homebudget/service/MutualFundServices.java
+++ src/main/java/com/homebudget/homebudget/service/MutualFundServices.java
@@ -4,6 +4,7 @@ import com.homebudget.homebudget.entity.Insurance;
 import com.homebudget.homebudget.entity.Investment;
 import com.homebudget.homebudget.entity.MutualFund;
 import com.homebudget.homebudget.entity.User;
+import com.homebudget.homebudget.exceptions.BadRequestException;
 import com.homebudget.homebudget.repository.MutualRepository;
 import com.homebudget.homebudget.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,35 +20,35 @@ public class MutualFundServices {
     @Autowired
     private UserRepository userRepository;
     public MutualFund saveMutualFund(Long userId, MutualFund mf) throws Exception {
-        User user = userRepository.findById(userId).get();
+        User user = this.userRepository.findById(userId).orElseThrow(()-> new BadRequestException("User not found for id "+userId ));
         Investment investment  = user.getInvestment();
         mf.setInvestment(investment);
         return this.mutualRepository.save(mf);
     }
 
-    public MutualFund getMutual(Long mutualId) throws Exception {
-        MutualFund mf = mutualRepository.findById(mutualId).orElse(null);
+    public MutualFund getMutual(Long mutualFundId) throws Exception {
+        MutualFund mf = this.mutualRepository.findById(mutualFundId).orElseThrow(()-> new BadRequestException("Mutual fund not found for id "+mutualFundId ));
         return mf;
     }
 
     public List<MutualFund> getAllMutualFundByUserId(Long userId) throws Exception{
-        User user = userRepository.findById(userId).get();
+        User user = this.userRepository.findById(userId).orElseThrow(()-> new BadRequestException("User not found for id "+userId ));
         Investment investment = user.getInvestment();
         return investment.getMutualFunds();
     }
-    public void deleteMutualFund(Long mutualId) throws Exception {
-        MutualFund stock = mutualRepository.findById(mutualId).orElse(null);
-        mutualRepository.deleteById(mutualId);
+    public void deleteMutualFund(Long mutualFundId) throws Exception {
+        MutualFund stock = this.mutualRepository.findById(mutualFundId).orElseThrow(()-> new BadRequestException("Mutual fund not found, id "+mutualFundId ));
+        this.mutualRepository.deleteById(mutualFundId);
     }
-    public MutualFund updateMutualFund(Long id, MutualFund updatedMutualFund) throws Exception {
-        MutualFund mf = mutualRepository.findById(id).get();
+    public MutualFund updateMutualFund(Long mutualFundId, MutualFund updatedMutualFund) throws Exception {
+        MutualFund mf = this.mutualRepository.findById(mutualFundId).orElseThrow(()-> new BadRequestException("Mutual fund not found, id "+mutualFundId ));
         mf.setName(updatedMutualFund.getName());
         mf.setAmount(updatedMutualFund.getAmount());
-        return mutualRepository.save(mf);
+        return this.mutualRepository.save(mf);
     }
 
     public int getTotalAmount(long userId) {
-        User user = this.userRepository.findById(userId).get();
+        User user = this.userRepository.findById(userId).orElseThrow(()-> new BadRequestException("User not found, id "+userId ));
         List<MutualFund> mfs = user.getInvestment().getMutualFunds();
         int totalAmount = 0;
         for(MutualFund mf : mfs) totalAmount+=mf.getAmount();
