58b9973fb093a3b6c86aceead916560c718aa74e
vaibhavgupta082 <vaibhavgupta2017082@gmail.com>
2023-02-18 19:15:09 +0530

    Adding exceptions to Insurance service

--- src/main/java/com/homebudget/homebudget/service/InsuranceServices.java
+++ src/main/java/com/homebudget/homebudget/service/InsuranceServices.java
@@ -4,6 +4,7 @@ import com.homebudget.homebudget.entity.Bank;
 import com.homebudget.homebudget.entity.Insurance;
 import com.homebudget.homebudget.entity.Investment;
 import com.homebudget.homebudget.entity.User;
+import com.homebudget.homebudget.exceptions.BadRequestException;
 import com.homebudget.homebudget.repository.InsuranceRepository;
 import com.homebudget.homebudget.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,34 +20,34 @@ public class InsuranceServices {
     private UserRepository userRepository;
 
     public Insurance saveInsurance(Long userId, Insurance insurance) throws Exception {
-        User user = userRepository.findById(userId).get();
+        User user = this.userRepository.findById(userId).orElseThrow(()-> new BadRequestException("user not found "+userId));
         Investment investment  = user.getInvestment();
         insurance.setInvestment(investment);
         return this.insuranceRepository.save(insurance);
     }
 
     public Insurance getInsurance(Long insuranceId) throws Exception {
-        Insurance insurance = insuranceRepository.findById(insuranceId).orElse(null);
+        Insurance insurance = this.insuranceRepository.findById(insuranceId).orElseThrow(()-> new BadRequestException("Insurance not found "+insuranceId));
         return insurance;
     }
 
     public List<Insurance> getAllInsuranceIdByUserId(Long userId) throws Exception{
-        User user = userRepository.findById(userId).get();
+        User user = this.userRepository.findById(userId).orElseThrow(()-> new BadRequestException("User not found "+userId));
         Investment investment = user.getInvestment();
         return investment.getInsurances();
     }
     public void deleteInsurance(Long insuranceId) throws Exception {
-        Insurance insurance = insuranceRepository.findById(insuranceId).orElse(null);
-        insuranceRepository.deleteById(insuranceId);
+        Insurance insurance = this.insuranceRepository.findById(insuranceId).orElseThrow(()-> new BadRequestException("Insurance not found "+insuranceId));
+        this.insuranceRepository.deleteById(insuranceId);
     }
     public Insurance updateInsurance(Long id, Insurance updatedInsurance) throws Exception {
-        Insurance insurance = insuranceRepository.findById(id).get();
+        Insurance insurance = this.insuranceRepository.findById(id).orElseThrow(()-> new BadRequestException("Insurance not found "+id));
         insurance.setName(updatedInsurance.getName());
         insurance.setAmount(updatedInsurance.getAmount());
-        return insuranceRepository.save(insurance);
+        return this.insuranceRepository.save(insurance);
     }
     public int getTotalAmount(long userId) {
-        User user = this.userRepository.findById(userId).get();
+        User user = this.userRepository.findById(userId).orElseThrow(()-> new BadRequestException("User not found "+userId));
         List<Insurance> insurances = user.getInvestment().getInsurances();
         int totalAmount = 0;
         for(Insurance ins : insurances) totalAmount+=ins.getAmount();
